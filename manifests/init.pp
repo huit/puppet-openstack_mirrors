e117aac7558d7756eeaebeaa2ed9cf98f1c607ac
Steve Huff <shuff@vecna.org>
2014-02-18 10:40:50 -0500

    try overriding some mrepo params

--- manifests/init.pp
+++ manifests/init.pp
@@ -51,6 +51,13 @@ class openstack_mirrors (
 
   class { 'mrepo': }
 
+  class { 'mrepo::params':
+    src_root    => '/opt/mrepo',
+    www_root    => '/opt/www/mrepo',
+    http_proxy  => 'http://proxy.noc.harvard.edu:3128',
+    https_proxy => 'http://proxy.noc.harvard.edu:3128',
+  }
+
   if $openstack_mirrors::el_versions {
     case $openstack_mirrors::el_source {
       'centos': {
