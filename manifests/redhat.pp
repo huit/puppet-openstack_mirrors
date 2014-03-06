ecc1ab09d38ab2ae86f6ee688bc7cebe2daca107
Steve Huff <shuff@vecna.org>
2014-03-06 11:13:17 -0500

    Docs and code out of sync

--- manifests/redhat.pp
+++ manifests/redhat.pp
@@ -2,31 +2,26 @@ define openstack_mirrors::redhat (
 ) {
   validate_re(
     $title,
-    '^[65]-[[:alnum:]_]+$',
+    '^\d+-[[:alnum:]_]+$',
     "'${title}' is not of the form '6-x86_64'."
   )
 
   $releasearch = split($title, '-')
   validate_array($releasearch)
 
-  $rhnreleases = {
-    '6' => '6Server',
-    '5' => '5Server',
-  }
-
   $release = $releasearch[0]
   $rhnrelease = $rhnreleases[$release]
   $arch = $releasearch[1]
 
   mrepo::repo { "redhat-${release}-${arch}":
-    ensure     => 'present',
-    require    => Class['mrepo'],
-    update     => 'nightly',
-    repotitle  => "Red Hat Enterprise Linux ${release} ${arch}",
-    release    => $release,
-    rhnrelease => $rhnrelease,
-    arch       => $arch,
-    urls       => {
+    ensure    => 'present',
+    require   => Class['mrepo'],
+    update    => 'nightly',
+    repotitle => "Red Hat Enterprise Linux ${release} ${arch}",
+    release   => $release,
+    type      => 'rhn',
+    arch      => $arch,
+    urls      => {
       "rhel${release}-${arch}" => "rhns:///rhel-${arch}-server-${release}",
     },
   }
